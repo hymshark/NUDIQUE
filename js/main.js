0b7c6b5fd173f42deb5026e18098e277bc5d5a83
hyemshark <eclatant79@maver.com>
2025-03-08 22:20:42 +0900

    파일옮기기

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(function(){
+   $('.header').load('/NUDIQUE/common/header.html'); //스크립트 작성 시 문제 발생됨. 경로부분을 없다고 인식함->경로를 common.js에 연결시켜둔다.
+   $('.footer').load('/NUDIQUE/common/footer.html');
+});//script end 
